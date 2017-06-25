a852bff46d91a1ae0bf409f5a150ad284adafd37
Griffith Chaffee <griffithchaffee@gmail.com>
2017-06-25 06:30:02 -0700

    added remote_ip session tracking

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
   #protect_from_forgery with: :exception
+  prepend_before_action :autocreate_session
+
+  def autocreate_session
+    session[:remote_ip] = request.remote_ip
+    session
+  end
 
   def is_admin?
     if Rails.env.development?
@@ -8,6 +14,6 @@ class ApplicationController < ActionController::Base
       request.remote_ip == Rails.application.settings.admin_ip
     end
   end
-
   helper_method :is_admin?
+
 end
