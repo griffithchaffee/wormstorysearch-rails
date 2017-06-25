3474e702db5ab2da59f91b01e0da5c4313ed3166
Griffith Chaffee <griffithchaffee@gmail.com>
2017-06-25 04:22:22 -0700

    removed debug message

--- app/models/identity_session.rb
+++ app/models/identity_session.rb
@@ -15,13 +15,12 @@ class IdentitySession < ApplicationRecord
 
   # public/private/protected/classes
   def initialize(attributes_hash, options_hash = {})
-    Rails.logger.fatal { "IdentitySession.new(#{attributes_hash}, #{options_hash})" }
     raise "session options_hash: #{options_hash.inspect}" if options_hash.present?
     super(attributes_hash.to_h)
   end
 
-  define_method("data=") { |hash| self[:data] = hash.to_h.with_indifferent_access }
-  define_method(:data) { (self[:data] || {}).to_h.with_indifferent_access }
+  define_method(:data=) { |hash| self[:data] = hash.to_h.with_indifferent_access }
+  define_method(:data)  { (self[:data] || {}).to_h.with_indifferent_access }
 
 end
 
