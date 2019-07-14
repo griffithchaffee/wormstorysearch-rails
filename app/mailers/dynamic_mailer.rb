f61190c6580f520416d8ad342af1488c092a6bdf
Griffith Chaffee <griffithchaffee@gmail.com>
2019-07-13 20:20:32 -0700

    log email subject

--- app/mailers/dynamic_mailer.rb
+++ app/mailers/dynamic_mailer.rb
@@ -4,6 +4,7 @@ class DynamicMailer < ApplicationMailer
     params = params.with_indifferent_access.assert_valid_keys(*%w[ reply_to subject body to ])
     params = params.select { |k,v| v.present? }.reverse_merge(to: "wormstorysearch@gmail.com")
     @body = params.delete(:body)
+    Rails.logger.info { params[:subject] }
     mail(params)
   end
 
