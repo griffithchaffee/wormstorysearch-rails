f41afb43318962a398c68010ca177b9db545cb57
Griffith Chaffee <griffithchaffee@gmail.com>
2019-09-11 10:52:44 -0700

    added admin_ip_address logic

--- app/controllers/application_controller/admin.rb
+++ app/controllers/application_controller/admin.rb
@@ -1,11 +1,26 @@
 class ApplicationController < ActionController::Base
 
+  before_action(:set_admin_status)
+
+  def set_admin_status
+    session[:is_admin] = "true" if is_admin_passphrase?
+    is_admin?
+  end
+
   def is_admin?
-    return true if Rails.env.development?
-    session[:is_admin] = "true" if params[:passphrase] == ENV["ADMIN_PASSPHRASE"]
     session[:is_admin].to_s == "true"
   end
-  helper_method :is_admin?
+  helper_method(:is_admin?)
+
+  def is_admin_passphrase?
+    params[:passphrase] == ENV["ADMIN_PASSPHRASE"]
+  end
+  helper_method(:is_admin_passphrase?)
+
+  def is_admin_ip_address?
+    request.remote_ip.in?(ENV["ADMIN_IP_ADDRESSES"].split(" "))
+  end
+  helper_method(:is_admin_ip_address?)
 
   def admin_only_action
     if !is_admin?
