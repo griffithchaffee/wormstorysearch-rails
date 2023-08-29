2ae9bacb3d4b55af19e130b3da3c63193930b410
Griffith Chaffee <griffithchaffee@gmail.com>
2023-08-29 07:52:00 +0000

    removed old comment

--- lib/rails/configurations/insert_middleware.rb
+++ lib/rails/configurations/insert_middleware.rb
@@ -1,6 +1,4 @@
 Rails.application.configure do
-  # Rack::Lock prevents threaded requests which break database connections
-  #config.app_middleware.insert_after(ActionDispatch::Static, Rack::Lock)
   # add custom middleware
   config.app_middleware.insert_before(Rails::Rack::Logger, Rails.application.class::Middleware::LoggerSilencer)
   config.app_middleware.insert_before(Rails::Rack::Logger, Rails.application.class::Middleware::VulnerabilityScanning)
